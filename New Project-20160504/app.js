42a9a89c6f1992c26eb0bad5b8dadbb376569ab6
Zubair <zeewebdeveloper0015@gmail.com>
2016-05-04 10:56:16 +0530

    Created by Coding Ground Client

---
+++ New Project-20160504/app.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function(req,res){
+    if(req.url === '/favicon.ico'){
+        return res.end();
+    }
+    
+    console.log("Incoming Request To " + req.url);
+    
+    var i = 2;
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    setTimeout(function(){
+        fs.readFile(__filenam, {
+            encoding: 'utf-8'
+        }, function(error, contents){
+            if(error){
+                console.error(error);
+                return res.end();
+            }
+          console.log('Sending Response for ' + req.url);
+          res.end(contents);
+          
+        });
+    }, 5000);
+    
+    while(i--){
+        console.log('Loop value ' + i + '\r' );
+    }
+}).listen(1337,'127.0.0.1');
+
+console.log('Server Running at http://127.0.0.1:1337/');
